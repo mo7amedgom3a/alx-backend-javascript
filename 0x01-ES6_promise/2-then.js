f339d2c04077ece78fbca00c26cbb238237aa448
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-07-25 01:37:34 +0300

    test

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
-export default function handleResponseFromAPI(promise){
+/* eslint-disable no-console */
+export default function handleResponseFromAPI(promise) {
     return promise
-    .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
-}
+      .then(() => ({ status: 200, body: 'success' }))
+      .catch(() => Error())
+      .finally(() => { console.log('Got a response from the API'); });
+  }
