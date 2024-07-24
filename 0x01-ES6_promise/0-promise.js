887d87f4965f7f48aeac0556730fcd411813c950
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-07-25 01:11:09 +0300

    test

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
-function getResponseFromAPI(){
-    return new Promise((resolve, reject) => {});
+/* eslint-disable */
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {});
 }
-export default getResponseFromAPI;
